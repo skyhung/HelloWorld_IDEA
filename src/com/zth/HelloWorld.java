d1a2ce617a29578b36c6cfcda7b4b869315da880
skyhung <460363642@qq.com>
2019-07-26 17:42:49 +0800

    Initial commit

---
+++ src/com/zth/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.zth;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("start");
+
+        System.out.println("start2");
+
+    }
+
+    public void main2(){
+
+
+    }
+}
---
+++ src/com/zth/Test2.java
@@ -0,0 +1,11 @@
+package com.zth;
+
+/**
+ * @author zth
+ * @date 2019/7/25 16:09
+ */
+public class Test2 {
+    public static void main(String[] args) {
+
+    }
+}
